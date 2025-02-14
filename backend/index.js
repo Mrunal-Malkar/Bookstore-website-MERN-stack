17e31a3630db92f1c165b1b6e72c5f512f87e7f0
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-02-14 17:28:22 +0530

    deployment changes in server

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,18 @@ import cartRoute from "./routes/cart.route.js"
 
 const app=express();
 
+
 dotenv.config();
 app.use(cors())
 app.use(express.json());
 
 mongoDbConnect()
 
+const PORT=process.env.PORT || 3000
+
 app.use("/store",bookRoute);
 app.use("/cart",cartRoute);
 
-app.listen("3000",()=>{
+app.listen(PORT,()=>{
     console.log("server is running on port 3000");
 })
