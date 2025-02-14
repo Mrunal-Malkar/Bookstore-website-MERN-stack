a5a06ca77b867ae5a912a44fb2da0909685b955f
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-02-14 18:34:52 +0530

    trying to fix backend error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ const app=express();
 
 
 dotenv.config();
-app.use(cors({ origin: "https://bookstore-website-mern-stack.vercel.app"}));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }));
 app.use(express.json());
 
 mongoDbConnect()
